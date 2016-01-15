79ffaabb28dc9efe1845261ee3613502c1aa957a
Matthew Schaefer <m406605@a4370389.federated.fds>
2016-01-15 14:49:13 -0800

    some stuff

--- hamster.js
+++ hamster.js
@@ -8,8 +8,8 @@ function Hamster (name, breed, age, sound, imgUrl) {
 }
 
 // dog Instances
-var cousteau = new Hamster('Chiquita Banana', 'regular', 14, 'squeek','https://forcechange.com/wp-content/uploads/2015/05/syrian-hamster.jpg');
-var Emy = new Hamster('Emy','regular', 7, 'AAAAAHHH!','http://www.yourpurebredpuppy.com/dogbreeds/photos-AB/australianshepherds-th1.jpg' );
+var cousteau = new Hamster('Cousteau', 'regular', 14, 'zzzzzzzzzzzz','https://forcechange.com/wp-content/uploads/2015/05/syrian-hamster.jpg');
+var fred = new Hamster('Fred','little', 7, 'AAAAAHHH!','https://s-media-cache-ak0.pinimg.com/736x/6d/e2/25/6de2251b8b4be709dcc936ae4f0caaaf.jpg' );
 
 
 // hamster Array
@@ -24,7 +24,7 @@ for (i=0;i < hamsterArray.length; i++) {
         var animalDiv = document.createElement('div');
         animalDiv.className = 'animalContainer'        
         animalDiv.innerHTML = '<div class="animalContent"><img class="animalImg" src="'+img+'" /><p class="name">'+name+'</p><p class="breed">'+breed+'</p><p class="sound">'+sound+'</p></div>';
-        var sectionContainer = document.getElementById('dogs');
+        var sectionContainer = document.getElementById('hamsters');
         sectionContainer.appendChild(animalDiv);
 }
 
